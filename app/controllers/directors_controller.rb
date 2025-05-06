0853dcfe5625283946a7ce2c271a2f58ff4fb8a1
hyubooth <hyu80@uchicago.edu>
2025-05-06 01:07:12 +0000

    finished

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,5 +2,25 @@ class DirectorsController < ApplicationController
   def index
     render ({:template => "director_templates/list"})
   end
+  def show
+    the_id = params.fetch("the_id")
+
+    matching_records = Director.where({:id => the_id})
+
+    @the_director = matching_records.at(0)
+
+    render ({:template => "director_templates/details"})
+  end
+
+  def youngest
+    @youngest_director = Director.order(:dob).last
+    render({ :template => "director_templates/youngest" })
+  end
+  
+  def eldest
+    @eldest_director = Director.order(:dob).first
+    render({ :template => "director_templates/eldest" })
+  end
+  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,21 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index" })
 
+  
+
+  get("/movies", { :controller => "movies", :action => "index" })
+
+  get("/movies/:the_id", { :controller => "movies", :action => "show" })
+
+  get("/actors", { :controller => "actors", :action => "index" })
+
+  get("/actors/:the_id", { :controller => "actors", :action => "show" })
+
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+
+  get("/directors/eldest", { :controller => "directors", :action => "eldest" })
+
+  get("/directors/:the_id", { :controller => "directors", :action => "show" })
+
 
 end
